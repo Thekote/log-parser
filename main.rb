656c57d3cb538c997d00d53d30a66f1d20715cc7
Thekote <dkglima@gmail.com>
2022-01-18 12:25:12 -0300

    fix: refactor method to use p instead of puts

--- main.rb
+++ main.rb
@@ -2,8 +2,9 @@ class Main
   def read_first_line(file_name)
     file = File.open(file_name)
     first_line = file.readline
-    puts first_line
     file.close
+    p first_line
   end
 end
 Main.new.read_first_line("games.log")
+
