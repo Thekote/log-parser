5cb8d2f5088411c18051a0c24c9ae95b4ef1565b
Thekote <dkglima@gmail.com>
2022-01-18 18:10:14 -0300

    feat: add test for input file error and test for game_test mock file

--- spec/read_first_line_spec.rb
+++ spec/read_first_line_spec.rb
@@ -1,8 +1,13 @@
-require_relative '../main'
+require_relative '../libs/log_parser.rb'
 
-describe Main do
-  it 'should return first line from file' do
-    expect(Main.new.read_first_line('games.log')).to eq("  0:00 ------------------------------------------------------------\n")
+describe LogParser do
+  describe '#read_first_line' do
+    it 'should return first line from file' do
+      expect(LogParser.new.read_first_line('./spec/fixtures/game_test.log')).to eq("  0:00 ------------------------------------------------------------\n")
+    end
+    it 'should return an error if file does not exists' do
+    expect{LogParser.new.read_first_line('nonExistingFile.log')}.to raise_error("input file not found or does not exists")
+    end
   end
 end
 
