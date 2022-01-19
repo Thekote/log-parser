8c5c8fb576e7a484860c009c541d89cba59f6671
Thekote <dkglima@gmail.com>
2022-01-19 17:50:16 -0300

    feat: add tests for parse_file and count_lines methods

--- spec/read_first_line_spec.rb
+++ spec/read_first_line_spec.rb
@@ -1,3 +1,4 @@
+require 'json'
 require_relative '../libs/log_parser.rb'
 
 describe LogParser do
@@ -10,6 +11,21 @@ describe LogParser do
     it 'should return an error if file does not exists' do
       parser = LogParser.new('nonExistingFile.log')
       expect{parser.read_first_line}.to raise_error("input file not found or does not exists")
+    end
+  end
+
+  describe '#parse_file' do
+    it 'should return a json when called' do
+      parser = LogParser.new('./spec/fixtures/game_test.log')
+      obj = {"./spec/fixtures/game_test.log": {"lines": 11}}
+      expect(parser.parse_file).to eq(JSON.pretty_generate(obj))
+    end
+  end
+
+  describe '#count_lines' do
+    it 'should not fail if the number of lines is correct' do
+      parser = LogParser.new('./spec/fixtures/game_test.log')
+      expect(parser.count_lines).to match(11)
     end
   end
 end
