59dd803ad594f8cd5358767e63226ac72102cb19
Thekote <dkglima@gmail.com>
2022-01-18 22:45:34 -0300

    fix: remove print method from read_first_line

--- libs/log_parser.rb
+++ libs/log_parser.rb
@@ -5,7 +5,7 @@ class LogParser
       file = File.open(file_name)
       first_line = file.readline
       file.close
-      p first_line
+      first_line
     rescue Errno::ENOENT
       raise "input file not found or does not exists"
     end
