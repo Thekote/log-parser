2e1fac4da55e52feaa816e5882e7f9bc50e7d898
Thekote <dkglima@gmail.com>
2022-01-18 23:32:49 -0300

    fix: tests to match log_parser refactor

--- spec/read_first_line_spec.rb
+++ spec/read_first_line_spec.rb
@@ -3,11 +3,13 @@ require_relative '../libs/log_parser.rb'
 describe LogParser do
   describe '#read_first_line' do
     it 'should return first line from file' do
-      expect(LogParser.new.read_first_line('./spec/fixtures/game_test.log')).to eq("  0:00 ------------------------------------------------------------\n")
+      parser = LogParser.new('./spec/fixtures/game_test.log')
+      expect(parser.read_first_line).to eq("  0:00 ------------------------------------------------------------\n")
     end
 
     it 'should return an error if file does not exists' do
-    expect{LogParser.new.read_first_line('nonExistingFile.log')}.to raise_error("input file not found or does not exists")
+      parser = LogParser.new('nonExistingFile.log')
+      expect{parser.read_first_line}.to raise_error("input file not found or does not exists")
     end
   end
 end
