aa83dc603ef10635b844cd2fff2f877d6953181d
Thekote <dkglima@gmail.com>
2022-01-18 22:40:55 -0300

    fix: add one line for better code reading

--- spec/read_first_line_spec.rb
+++ spec/read_first_line_spec.rb
@@ -5,6 +5,7 @@ describe LogParser do
     it 'should return first line from file' do
       expect(LogParser.new.read_first_line('./spec/fixtures/game_test.log')).to eq("  0:00 ------------------------------------------------------------\n")
     end
+
     it 'should return an error if file does not exists' do
     expect{LogParser.new.read_first_line('nonExistingFile.log')}.to raise_error("input file not found or does not exists")
     end
